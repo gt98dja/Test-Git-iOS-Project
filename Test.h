d93a3a09d8e18e9ad8b9cf4b33f4e56196daa354
Daniel Jansson <danja612@MacBook-Pro.local>
2014-04-15 16:28:40 +0200

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git test
+//
+//  Created by Daniel Jansson on 2014-04-15.
+//  Copyright (c) 2014 Daniel Jansson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git test
+//
+//  Created by Daniel Jansson on 2014-04-15.
+//  Copyright (c) 2014 Daniel Jansson. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
