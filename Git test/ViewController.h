a48f11bea87cce2f5b5aba457ae9adef93f5a86b
Daniel Jansson <danja612@MacBook-Pro.local>
2014-04-15 14:56:05 +0200

    Initial Commit

---
+++ Git test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Git test
+//
+//  Created by Daniel Jansson on 2014-04-15.
+//  Copyright (c) 2014 Daniel Jansson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Git testTests/Git_testTests.m
@@ -0,0 +1,34 @@
+//
+//  Git_testTests.m
+//  Git testTests
+//
+//  Created by Daniel Jansson on 2014-04-15.
+//  Copyright (c) 2014 Daniel Jansson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Git_testTests : XCTestCase
+
+@end
+
+@implementation Git_testTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
